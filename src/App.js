fac8cf25168b8975ea76527fc45d9e8a6ee2fdde
Chacko jithin <jithinchackopayyanat@gmail.com>
2022-11-25 14:09:41 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
 import './App.css';
-
+// import Header from './Components/Header/Header1';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Pages/Home';
+import Dashboard from './Pages/Dashboard';
+import CoinPages from './Pages/CoinPages';
 function App() {
   return (
-    <div >
-      
-    </div>
+     <BrowserRouter>
+       <Routes>
+         <Route path='/' element={<Home/>}/>
+         <Route path='/dashboard' element={<Dashboard/>}/>
+         <Route path='/coin' element={<CoinPages/>}/>
+       </Routes>
+     </BrowserRouter>
   );
 }
 
